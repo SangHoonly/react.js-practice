f25651bb2b0a912ddd1a09158b8eb3d724b16871
Blueberry <54464320+lee01097@users.noreply.github.com>
2020-12-30 02:05:02 +0900

    #2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  const name = "리액트";
+
+  return (
+    <Fragment>
+      <div className="react">{name}</div>
+      <input></input>
+    </Fragment>
+  );
+}
+export default App;
