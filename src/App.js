9c5d74ed15ad0c5926d1e3955ff3053633678245
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-01-05 13:35:39 +0900

    useState practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Fragment } from "react";
 import "./App.css";
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
-  return (
-    <MyComponent name="React" age="28" address="대구시 수성구" favoriteNum="24">
-      리액트
-    </MyComponent>
-  );
+  return <Say />;
 };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -5,14 +5,37 @@ class Counter extends Component {
     super(props);
     this.state = {
       number: 0,
+      fixedNumber: 10,
     };
   }
+
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>fixed = {fixedNumber}</h2>
+        <button
+          onClick={() => {
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+          }}
+        >
+          + 1
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ number: number - 1 });
+          }}
+        >
+          -1
+        </button>
       </div>
     );
   }
 }
+
+export default Counter;
