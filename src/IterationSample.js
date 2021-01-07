4367ffaa2acd797aa4a16f1f174447f91b4ce34e
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-01-07 23:46:35 +0900

    component iterating2

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,6 +11,15 @@ const IterationSample = () => {
   const [nextId, setNextId] = useState(5);
 
   const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
 
   const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
   return (
