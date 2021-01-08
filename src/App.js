ccb2f0cc80c8703918d6bac7f71cb41dc90f7d0f
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-01-08 23:11:27 +0900

    ~Life Cycle method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Component, Fragment } from "react";
 import "./App.css";
 import Counter from "./Counter";
 import Say from "./Say";
@@ -6,9 +6,30 @@ import EventPractice from "./EventPractice";
 import Clock from "./Clock";
 import MyComponent from "./MyComponent";
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
-const App = () => {
-  return <IterationSample />;
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,22 +2,16 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class MyComponent extends Component {
-  state = { message: "하이요" };
-
-  handleClick = () => {
-    alert(this.state.message);
-    this.setState({ message: "바이요" });
-  };
-
   render() {
+    const { name, age } = this.props;
+
     return (
       <div>
-        <h1>안녕하세요</h1>
-        <h2>버튼을 누르세요</h2>
-        <button onClick={this.handleClick}>버튼클릭!</button>
+        <h1>안녕하세요. {name}입니다.</h1>
+        <h6>저는 {age}살 입니다.</h6>
+        <h3>{console.log(this)}</h3>
       </div>
     );
   }
 }
-
 export default MyComponent;
